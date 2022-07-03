76f0bc1dc6a9fb855262c061f58b4e1ae9f9e9f7
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-07-03 21:34:02 +0900

    困った

--- bot/code/index.js
+++ bot/code/index.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
 
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
-  if (!message.channel.id == '968100689177870376') return;
+  if (!message.channel.id == 968100689177870376) return;
 
   message.reply('明日の時間割は、');
   var time = 0;
