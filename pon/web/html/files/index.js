75129811270edd2624da6ab39bf8b49ef90e9412
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-07-29 20:33:29 +0900

    再生部分をちょっと変更

--- pon/web/html/files/index.js
+++ pon/web/html/files/index.js
@@ -1,22 +1,31 @@
-const a1 = new Audio('../sounds/audio1.mp4');
-const a2 = new Audio('../sounds/audio2.mp4');
-const a3 = new Audio('../sounds/audio3.mp4');
-const a4 = new Audio('../sounds/audio4.mp4');
-const a5 = new Audio('../sounds/audio5.mp4');
+const a1 = new Audio('../upload/mp4/audio1.mp4');
+const a2 = new Audio('../upload/mp4/audio2.mp4');
+const a3 = new Audio('../upload/mp4/audio3.mp4');
+const a4 = new Audio('../upload/mp4/audio4.mp4');
+const a5 = new Audio('../upload/mp4/audio5.mp4');
 
 function s1() {
+    a1.volume = 0.5
     a1.play();
 }
 function s2() {
+    a2.volume = 0.5
     a2.play();
 }
 function s3() {
+    a3.volume = 0.5
+    a3.play();
+}
+function s3_big() {
+    a3.volume = 1.0
     a3.play();
 }
 function s4() {
+    a4.volume = 0.5
     a4.play();
 }
 function s5() {
+    a5.volume = 0.5
     a5.play();
 }
 function pause() {
