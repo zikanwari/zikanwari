420c43addfc3df8b5a60611da32f888e25431aa8
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-15 22:43:06 +0900

    API開発

---
+++ web/html/api/all.php
@@ -0,0 +1,33 @@
+<?php
+try{
+    $pdo = new PDO(
+        'mysql:host=' . getenv('DB') . ';dbname=zikan;charset=utf8',
+        getenv('USER'),
+        getenv('PASSWORD')
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+}catch(PDOException $Exception){
+    die('エラー,エラー,エラー,エラー,エラー,' .$Exception->getMessage());
+}
+try{
+    $sql = "SELECT * FROM zikan.`2-5`";
+    $stmh = $pdo->prepare($sql);
+    $stmh->execute();
+}catch(PDOException $Exception){
+    die('エラー,エラー,エラー,エラー,エラー,' .$Exception->getMessage());
+}
+
+$week = [
+    '月',
+    '火',
+    '水',
+    '木',
+    '金',
+  ];
+
+  for ($i=0; $i < 5; $i++) { 
+    while($row = $stmh->fetch(PDO::FETCH_ASSOC)){
+        echo ( htmlspecialchars($row[ $week[i] . '曜日' ]) . ',' );
+    }
+  }
