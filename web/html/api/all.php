e57ec85cf005b2a554baadf9a91983495123b0eb
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-18 09:43:58 +0900

    忘れてた

--- web/html/api/all.php
+++ web/html/api/all.php
@@ -19,9 +19,9 @@ try{
 }
 
 while($row = $stmh->fetch(PDO::FETCH_ASSOC)){
-    echo (htmlspecialchars($row['月曜日']));
-    echo (htmlspecialchars($row['火曜日']));
-    echo (htmlspecialchars($row['水曜日']));
-    echo (htmlspecialchars($row['木曜日']));
-    echo (htmlspecialchars($row['金曜日']));
+    echo ( htmlspecialchars($row['月曜日']) . ',' );
+    echo ( htmlspecialchars($row['火曜日']) . ',' );
+    echo ( htmlspecialchars($row['水曜日']) . ',' );
+    echo ( htmlspecialchars($row['木曜日']) . ',' );
+    echo ( htmlspecialchars($row['金曜日']) . ',' );
 }
