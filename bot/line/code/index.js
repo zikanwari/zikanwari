6070352907c0f07d206cc0ac23d591149f7e436b
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-18 17:06:31 +0900

    コピペで復元

--- bot/line/code/index.js
+++ bot/line/code/index.js
@@ -7,8 +7,8 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const TOKEN = process.env.LINE_ACCESS_TOKEN
 
-let msg1;
-let msg2;
+var msg1;
+var msg2;
 
 app.use(express.json())
 app.use(express.urlencoded({
@@ -21,30 +21,29 @@ app.get("/", (req, res) => {
 
 app.post("/webhook", function(req, res) {
   res.send("HTTP POST request sent to the webhook URL!")
-  console.log('webhook recieved');
   if (req.body.events[0].type === "message") {
-    console.log('msg recieved'); 
     var time = 0;
   
     zikan_request.get({
-      uri: URL,
-      headers: {'Content-type': 'application/json'},
-    }, function(err, req, data){
+    uri: URL,
+    headers: {'Content-type': 'application/json'},
+  }, function(err, req, data){
     a = data.split(',');
     a.pop();
     msg1 = '明日(' + a[6] + ')の時間割は、';
     for(x in a){
 
         sub = a[x];
+
         time++;
+
         if (time > 6) {
           break;
         }
-        msg2 += time + '時間目：' + sub;
-      }});
 
-      console.log(msg1);
-      console.log(msg2);
+        msg2 += time + '時間目：' + sub;
+
+      }});
     const dataString = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
       messages: [
@@ -58,11 +57,13 @@ app.post("/webhook", function(req, res) {
         }
       ]
     })
+
     // リクエストヘッダー
     const headers = {
       "Content-Type": "application/json",
       "Authorization": "Bearer " + TOKEN
     }
+
     // リクエストに渡すオプション
     const webhookOptions = {
       "hostname": "api.line.me",
@@ -71,21 +72,25 @@ app.post("/webhook", function(req, res) {
       "headers": headers,
       "body": dataString
     }
+
     // リクエストの定義
     const request = https.request(webhookOptions, (res) => {
       res.on("data", (d) => {
         process.stdout.write(d)
       })
     })
+
     // エラーをハンドル
     request.on("error", (err) => {
       console.error(err)
     })
+
     // データを送信
     request.write(dataString)
     request.end()
   }
 })
+
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
