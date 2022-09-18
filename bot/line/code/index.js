feee6b7dd90e39b42787138776930e3650f154af
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-18 15:16:58 +0900

    訂正

--- bot/line/code/index.js
+++ bot/line/code/index.js
@@ -14,54 +14,54 @@ app.get("/", (req, res) => {
 })
 
 app.post("/webhook", function(req, res) {
-    res.send("HTTP POST request sent to the webhook URL!")
-    if (req.body.events[0].type === "message") {
-      const dataString = JSON.stringify({
-        replyToken: req.body.events[0].replyToken,
-        messages: [
-          {
-            "type": "test",
-            "text": "Hello world"
-          },
-          {
-            "type": "text",
-            "text": "May I help you?"
-          }
-        ]
-      })
-  
-      // リクエストヘッダー
-      const headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + TOKEN
-      }
-  
-      // リクエストに渡すオプション
-      const webhookOptions = {
-        "hostname": "api.line.me",
-        "path": "/v2/bot/message/reply",
-        "method": "POST",
-        "headers": headers,
-        "body": dataString
-      }
-  
-      // リクエストの定義
-      const request = https.request(webhookOptions, (res) => {
-        res.on("data", (d) => {
-          process.stdout.write(d)
-        })
-      })
-  
-      // エラーをハンドル
-      request.on("error", (err) => {
-        console.error(err)
-      })
-  
-      // データを送信
-      request.write(dataString)
-      request.end()
+  res.send("HTTP POST request sent to the webhook URL!")
+  if (req.body.events[0].type === "message") {
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          "type": "text",
+          "text": "Hello, user"
+        },
+        {
+          "type": "text",
+          "text": "May I help you?"
+        }
+      ]
+    })
+
+    // リクエストヘッダー
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + TOKEN
     }
-  })
+
+    // リクエストに渡すオプション
+    const webhookOptions = {
+      "hostname": "api.line.me",
+      "path": "/v2/bot/message/reply",
+      "method": "POST",
+      "headers": headers,
+      "body": dataString
+    }
+
+    // リクエストの定義
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d)
+      })
+    })
+
+    // エラーをハンドル
+    request.on("error", (err) => {
+      console.error(err)
+    })
+
+    // データを送信
+    request.write(dataString)
+    request.end()
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
