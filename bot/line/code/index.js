b583324fd0948247283acef27b6a0981abe4aea8
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-18 15:52:49 +0900

    多分完成（どうせしてない）

--- bot/line/code/index.js
+++ bot/line/code/index.js
@@ -4,6 +4,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const TOKEN = process.env.LINE_ACCESS_TOKEN
 
+//honhe
+const request = require('request');
+const URL = 'http://zikanwari/api/tomorrow.php';
+
 app.use(express.json())
 app.use(express.urlencoded({
   extended: true
@@ -16,16 +20,39 @@ app.get("/", (req, res) => {
 app.post("/webhook", function(req, res) {
   res.send("HTTP POST request sent to the webhook URL!")
   if (req.body.events[0].type === "message") {
+    var time = 0;
+  
+  request.get({
+    uri: URL,
+    headers: {'Content-type': 'application/json'},
+  }, function(err, req, data){
+    a = data.split(',');
+    a.pop();
+    var msg1 = '明日(' + a[6] + ')の時間割は、';
+    var msg2;
+    for(x in a){
+
+        sub = a[x];
+
+        time++;
+
+        if (time > 6) {
+          break;
+        }
+
+        msg2 += time + '時間目：' + sub;
+
+      }});
     const dataString = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
       messages: [
         {
           "type": "text",
-          "text": "いつか時間割が出るようになるかもしれない"
+          "text": msg1
         },
         {
           "type": "text",
-          "text": "（かもしれない）"
+          "text": msg2
         }
       ]
     })
