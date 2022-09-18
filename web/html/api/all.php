b13497bab1dbc46c2a97af57c299317d382dd720
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-18 09:38:00 +0900

    何も考えない

--- web/html/api/all.php
+++ web/html/api/all.php
@@ -8,28 +8,18 @@ try{
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 }catch(PDOException $Exception){
-    die('エラー,エラー,エラー,エラー,エラー,' .$Exception->getMessage());
+    die('エラー,' .$Exception->getMessage());
 }
 try{
     $sql = "SELECT * FROM zikan.`2-5`";
     $stmh = $pdo->prepare($sql);
     $stmh->execute();
 }catch(PDOException $Exception){
-    die('エラー,エラー,エラー,エラー,エラー,' .$Exception->getMessage());
+    die('エラー,' .$Exception->getMessage());
 }
 
-$week = array(
-    '月',
-    '火',
-    '水',
-    '木',
-    '金'
-);
-
-  $x = 0;
-  while ($x < 5) { 
-    while($row = $stmh->fetch(PDO::FETCH_ASSOC)){
-        echo ( htmlspecialchars($row[ $week[$x] . '曜日' ]) . ',' );
-    }
-    $x++;
-  }
+echo (htmlspecialchars($row['月曜日']));
+echo (htmlspecialchars($row['火曜日']));
+echo (htmlspecialchars($row['水曜日']));
+echo (htmlspecialchars($row['木曜日']));
+echo (htmlspecialchars($row['金曜日']));
