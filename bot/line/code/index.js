d2926c6fc957db72e1e753016978e04186accb4a
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-18 20:47:53 +0900

    変えるところ間違えた

--- bot/line/code/index.js
+++ bot/line/code/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 app.post("/webhook", function(req, res) {
   res.send("HTTP POST request sent to the webhook URL!")
   // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
-  console.log(req.body);
+  console.log(req.body.events);
   if (req.body.events[0].type === "message") {
     getdata(send1, send2)
   }
