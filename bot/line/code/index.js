58d9b882fc394f5b2955924188b2ff293c68cb52
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-09-18 13:34:48 +0900

    LINEを開発する

---
+++ bot/line/code/index.js
@@ -0,0 +1,68 @@
+const https = require("https")
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+const TOKEN = process.env.LINE_ACCESS_TOKEN
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get("/", (req, res) => {
+  res.sendStatus(200)
+})
+
+app.post("/webhook", function(req, res) {
+    res.send("HTTP POST request sent to the webhook URL!")
+    if (req.body.events[0].type === "message") {
+      const dataString = JSON.stringify({
+        replyToken: req.body.events[0].replyToken,
+        messages: [
+          {
+            "type": "test",
+            "text": "Hello world"
+          },
+          {
+            "type": "text",
+            "text": "May I help you?"
+          }
+        ]
+      })
+  
+      // リクエストヘッダー
+      const headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + TOKEN
+      }
+  
+      // リクエストに渡すオプション
+      const webhookOptions = {
+        "hostname": "api.line.me",
+        "path": "/v2/bot/message/reply",
+        "method": "POST",
+        "headers": headers,
+        "body": dataString
+      }
+  
+      // リクエストの定義
+      const request = https.request(webhookOptions, (res) => {
+        res.on("data", (d) => {
+          process.stdout.write(d)
+        })
+      })
+  
+      // エラーをハンドル
+      request.on("error", (err) => {
+        console.error(err)
+      })
+  
+      // データを送信
+      request.write(dataString)
+      request.end()
+    }
+  })
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
