86e0258346852001dd7f6df7f325707af1d4b4f1
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-10-21 06:37:07 +0900

    適当

--- pon/web/html/files/index.js
+++ pon/web/html/files/index.js
@@ -1,8 +1,5 @@
 const a1 = new Audio('../upload/mp4/audio1.mp4');
 const a2 = new Audio('../upload/mp4/audio2.mp4');
-const a3 = new Audio('../upload/mp4/audio3.mp4');
-const a4 = new Audio('../upload/mp4/audio4.mp4');
-const a5 = new Audio('../upload/mp4/audio5.mp4');
 
 function s1() {
     a1.volume = 0.5
@@ -13,31 +10,15 @@ function s2() {
     a2.play();
 }
 function s3() {
-    a3.volume = 0.5
-    a3.play();
+    a2.volume += 0.5
 }
 function s3_big() {
-    a3.volume = 1.0
-    a3.play();
-}
-function s4() {
-    a4.volume = 0.5
-    a4.play();
-}
-function s5() {
-    a5.volume = 0.5
-    a5.play();
+    a2.volume -= 0.5
 }
 function pause() {
     a1.pause();
     a2.pause();
-    a3.pause();
-    a4.pause();
-    a5.pause();
     
     a1.currentTime = 0;
     a2.currentTime = 0;
-    a3.currentTime = 0;
-    a4.currentTime = 0;
-    a5.currentTime = 0;
 }
