11d7b2ea794816293754ab483fb632348331d6a9
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-11-25 20:03:51 +0900

    試作

---
+++ multisong/html/files/index.js
@@ -0,0 +1,17 @@
+const alt = new Audio('../mp3/alt.mp3');
+const sop = new Audio('../mp3/sop.mp3');
+const ten = new Audio('../mp3/ten.mp3');
+
+function play() {
+    alt.play();
+    sop.play();
+    ten.play();
+}
+function pause() {
+    alt.pause();
+    sop.pause();
+    ten.pause();
+    
+    /*a1.currentTime = 0;
+    a2.currentTime = 0;*/
+}
