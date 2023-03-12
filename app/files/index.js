a7a859ee8fc304bb220949dc637513baef9ac72e
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-03-12 10:11:22 +0900

    できた

--- app/files/index.js
+++
@@ -1,6 +0,0 @@
-fetch(`http://zikaa/api/all.php`, {
-    headers: {
-        'Content-type': 'application/json'
-    },
-    mode: 'cors'
-})
---
+++ app/index.js
@@ -0,0 +1,9 @@
+var el = document.createElement("script");
+el.src = "time/index.js";
+document.body.appendChild(el);
+
+fetch(`time/index.html`)
+.then(response => response.text())
+.then(data => {
+    document.getElementById('timetable').innerHTML = data;
+});
