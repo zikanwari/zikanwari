944b9362d78784c497519a9d23d6eaa37ebba2bf
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-03-13 21:18:20 +0900

    入れ替えてみる

--- app/html/time/index.js
+++ app/html/time/index.js
@@ -1,15 +1,13 @@
-console.log("hello");
 fetch(`https://zikanwari.f5.si/api/all.php`)
-.then(response => {
-    if( !res.status === 200 ) {
-        document.getElementById('timetable').innerText= 'エラーが発生しました。エラーコード：' + response.status;
-        exit;
-    }
-})
 .then(data => {
     a = data.split(',');
     a.pop();
     for (let index = 0; index < a.length; index++) {
         document.getElementById(index).innerText = a[index];
+    }
+})
+.then(response => {
+    if( !res.status === 200 ) {
+        document.getElementById('timetable').innerText= 'エラーが発生しました。エラーコード：' + response.status;
     }
 });
