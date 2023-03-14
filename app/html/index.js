6983b20fb4643da6f90233b3e7f188c87d3051b9
launchpencil <launchpencil@gmail.com>
2023-03-14 09:57:18 +0000

    おためし

--- app/html/index.js
+++ app/html/index.js
@@ -3,18 +3,20 @@ fetch(`time/index.html`)
 .then(data => {
     document.getElementById('timetable').innerHTML = data;
 });
-fetch(`https://zikanwari.f5.si/api/all.php`)
-.then(data => {
-    a = data.split(',');
-    a.pop();
-    for (let index = 0; index < a.length; index++) {
-        document.getElementById(index).innerText = a[index];
-    }
-    });
-.then(response => {
-    if( !response.ok ) {
-        document.getElementById('timetable').innerText = 'エラーが発生しました。エラーコード：' + response.status;
-        console.log('エラーが発生しました。エラーコード：' + response.status);
-        exit
-    }
-})
+function syncdata() {
+    fetch(`https://zikanwari.f5.si/api/all.php`)
+    .then(data => {
+        a = data.split(',');
+        a.pop();
+        for (let index = 0; index < a.length; index++) {
+            document.getElementById(index).innerText = a[index];
+        }
+        });
+    .then(response => {
+        if( !response.ok ) {
+            document.getElementById('timetable').innerText = 'エラーが発生しました。エラーコード：' + response.status;
+            console.log('エラーが発生しました。エラーコード：' + response.status);
+            exit;
+        }
+    })
+}
