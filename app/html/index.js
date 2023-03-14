27da965a0a0ace7f651552f7f70d93387f1652d9
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-03-14 19:54:29 +0900

    これで行けそう

--- app/html/index.js
+++ app/html/index.js
@@ -7,17 +7,15 @@ fetch(`time/index.html`)
 function syncdata() {
     fetch(`https://zikanwari.f5.si/api/all.php`)
     .then(data => {
-        a = data.split(',');
-        a.pop();
-        for (let index = 0; index < a.length; index++) {
-            document.getElementById(index).innerText = a[index];
-        }
-    })
-    .then(response => {
-        if( !response.ok ) {
-            document.getElementById('timetable').innerText = 'エラーが発生しました。エラーコード：' + response.status;
-            console.log('エラーが発生しました。エラーコード：' + response.status);
-            exit;
+        if (isNaN(data)) {
+            document.getElementById('timetable').innerText = 'エラーが発生しました。エラーコード：' + data.status;
+            console.log('エラーが発生しました。エラーコード：' + data.status);
+        } else {
+            a = data.split(',');
+            a.pop();
+            for (let index = 0; index < a.length; index++) {
+                document.getElementById(index).innerText = a[index];
+            }
         }
     })
     .catch(error => {
