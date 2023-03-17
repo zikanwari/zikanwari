30fee507141e5e85e7485861557ef4cae64d6929
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-03-17 21:52:10 +0900

    designed by chatgpt

--- app/html/index.js
+++ app/html/index.js
@@ -5,27 +5,14 @@ fetch(`time/index.html`)
     syncdata();
 });
 function syncdata() {
-    fetch(`https://zikanwari.f5.si/api/all.php`)
+    fetch(`https://api.launchpencil.f5.si/zikanwari/?user=2-5&pass=tokoroten`)
+    .then(response => response.text())
     .then(data => {
-        if (isNaN(data)) {
-            switch (data) {
-                case 401:
-                    document.getElementById('timetable').innerText = '認証に失敗しました。ユーザー名またはパスワードが間違っている、または設定されていません。';
-                    console.log('エラーが発生しました。エラーコード：' + data.status);
-                    break;
-            
-                default:
-                    document.getElementById('timetable').innerText = 'エラーが発生しました。エラーコード：' + data.status;
-                    console.log('エラーが発生しました。エラーコード：' + data.status);
-                    break;
-            }
-        } else {
             a = data.split(',');
             a.pop();
             for (let index = 0; index < a.length; index++) {
                 document.getElementById(index).innerText = a[index];
             }
-        }
     })
     .catch(error => {
         document.getElementById('timetable').innerText = 'エラーが発生しました。エラーコード：' + error;
