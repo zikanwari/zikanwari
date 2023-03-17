5c9cff0c850de6e7c1e7c45700228c61c3a5168f
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-03-17 22:30:19 +0900

    これでいいのね

--- app/html/sw.js
+++ app/html/sw.js
@@ -12,7 +12,7 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
     // 外部のリソースかどうかを判定する
-  if (event.request.url.startsWith('http')) {
+  if (event.request.url.startsWith('https://api.launchpencil.f5.si/zikanwari')) {
     // 外部のリソースの場合は、キャッシュに追加しない
     return;
   }
