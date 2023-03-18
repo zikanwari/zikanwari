f5afc7936061385347bf00ea0672a1b0f1b199cc
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-03-18 23:58:56 +0900

    できそう

--- app/html/other/index.js
+++ app/html/other/index.js
@@ -1,5 +1,8 @@
 function startmoni() {
   document.getElementById('setdata').addEventListener('click',() => {
     console.log('Hello!');
+
+    localStorage.setItem('user', document.getElementById("username").value);
+    localStorage.setItem('pass', document.getElementById("password").value);
   });
 }
