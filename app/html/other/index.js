22a402f4602c9ce6042b15c83c154f58cf0e58cd
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-03-19 09:29:30 +0900

    多分できた

--- app/html/other/index.js
+++ app/html/other/index.js
@@ -1,8 +1,8 @@
 function startmoni() {
   document.getElementById('setdata').addEventListener('click',() => {
-    console.log('Hello!');
 
     localStorage.setItem('user', document.getElementById("username").value);
     localStorage.setItem('pass', document.getElementById("password").value);
+    alert("保存しました。「時間割」から確認できます。")
   });
 }
