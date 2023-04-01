b79dcccc644264aeb2efcea000e3828c8b30f97a
launchpencil <launchpencil@gmail.com>
2023-04-01 12:11:39 +0900

    これでいいのか

--- api/code/index.js
+++ api/code/index.js
@@ -15,32 +15,63 @@ const server = http.createServer((req, res) => {
     database: 'zikan'
   });
 
-  connection.connect((err) => {
-    if (err) {
-      console.error('error connecting: ' + err.message);
-      res.end('エラー,' + err.message);
-      return;
-    }
-
-    const sql = "SELECT * FROM ??";
-
-    connection.query(sql, [username], (err, results, fields) => {
-        if (err) {
-            console.error('error querying: ' + err.stack);
-            res.write('エラー,' + err.message);
-            return;
-        }
-
-        results.forEach((row) => {
-            res.write(
-                `${row.月曜日},${row.火曜日},${row.水曜日},${row.木曜日},${row.金曜日},`
-              );
-        });
-
-        connection.end();
-        res.end();
+  if (req.url === '/change') {
+    const day = queryObject.day;
+    const time = queryObject.time;
+    const subject = queryObject.subject;
+    
+    connection.connect((err) => {
+      if (err) {
+        console.error('error connecting: ' + err.message);
+        res.end('エラー,' + err.message);
+        return;
+      }
+  
+      const sql = "UPDATE ?? SET ?? = ? WHERE `time` = ?";
+      const params = [username, day, subject, time];
+  
+      connection.query(sql, params, (err, results, fields) => {
+          if (err) {
+              console.error('error querying: ' + err.stack);
+              res.write('エラー,' + err.message);
+              return;
+          }
+  
+          res.write(day + '曜日の' + time + '時間目の教科を' + subject + 'に変更しました。');
+  
+          connection.end();
+          res.end();
+      });
     });
-  });
+
+  } else {
+    connection.connect((err) => {
+      if (err) {
+        console.error('error connecting: ' + err.message);
+        res.end('エラー,' + err.message);
+        return;
+      }
+  
+      const sql = "SELECT * FROM ??";
+  
+      connection.query(sql, [username], (err, results, fields) => {
+          if (err) {
+              console.error('error querying: ' + err.stack);
+              res.write('エラー,' + err.message);
+              return;
+          }
+  
+          results.forEach((row) => {
+              res.write(
+                  `${row.月曜日},${row.火曜日},${row.水曜日},${row.木曜日},${row.金曜日},`
+                );
+          });
+  
+          connection.end();
+          res.end();
+      });
+    });
+  }
 });
 
 server.listen(3000, () => {
