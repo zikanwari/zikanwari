a7aeff18a08061fc4e9d517387d55b38510b32a6
launchpencil <launchpencil@gmail.com>
2023-04-01 23:28:46 +0900

    テスト

--- api/code/index.js
+++ api/code/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
+  console.log(req.url)
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   const queryObject = url.parse(req.url, true).query;
   const username = queryObject.user;
