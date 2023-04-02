c420a152e40a681b7e801570f9775f2e34673fe2
launchpencil <launchpencil@gmail.com>
2023-04-02 22:59:22 +0900

    汚いけど動くのでヨシ！

--- app/html/time/index.js
+++ app/html/time/index.js
@@ -1,29 +1,5 @@
 function syncdata() {
-    const username = localStorage.getItem('user');
-    const password = localStorage.getItem('pass');
-
-    fetch(`https://api.launchpencil.f5.si/zikanwari/?user=` + username + `&pass=` + password)
-    .then(response => response.text())
-    .then(data => {
-            a = data.split(',');
-            if (a[0] == "エラー") {
-                if (a[1].startsWith("Access denied for user")) {
-                    document.getElementById('timetable').innerText = 
-                        '認証に失敗しました。ユーザー名またはパスワードが間違っているか設定されていません。';
-                        return;
-                }
-                document.getElementById('timetable').innerText = '時間割のデータ取得に失敗しました。\n エラーメッセージ：' + a[1];
-                return;
-            }
-            a.pop();
-            for (let index = 0; index < a.length; index++) {
-                document.getElementById(index).innerText = a[index];
-            }
-    })
-    .catch(error => {
-        document.getElementById('timetable').innerText = '時間割のデータ取得に失敗しました。';
-    });
-
+  update();
 
     const customSelects = document.querySelectorAll(".custom-select");
 
@@ -33,7 +9,6 @@ function syncdata() {
       select.addEventListener("change", function() {
         customSelect.classList.add("selected");
         
-        
         fetch(`https://api.launchpencil.f5.si/zikanwari/change/?user=${username}&pass=${password}&id=${index}&subject=${this.value}`)
         .then(response => response.text())
         .then(data => {
@@ -42,10 +17,39 @@ function syncdata() {
         .catch(error => {
             alert('時間割のデータ取得に失敗しました。');
         });
+
+        update();
       });
 
       customSelect.addEventListener("click", function() {
         this.classList.toggle("open");
       });
     });
+}
+
+function update() {
+  const username = localStorage.getItem('user');
+  const password = localStorage.getItem('pass');
+
+  fetch(`https://api.launchpencil.f5.si/zikanwari/?user=` + username + `&pass=` + password)
+  .then(response => response.text())
+  .then(data => {
+          a = data.split(',');
+          if (a[0] == "エラー") {
+              if (a[1].startsWith("Access denied for user")) {
+                  document.getElementById('timetable').innerText = 
+                      '認証に失敗しました。ユーザー名またはパスワードが間違っているか設定されていません。';
+                      return;
+              }
+              document.getElementById('timetable').innerText = '時間割のデータ取得に失敗しました。\n エラーメッセージ：' + a[1];
+              return;
+          }
+          a.pop();
+          for (let index = 0; index < a.length; index++) {
+              document.getElementById(index).innerText = a[index];
+          }
+  })
+  .catch(error => {
+      document.getElementById('timetable').innerText = '時間割のデータ取得に失敗しました。';
+  });
 }
