066abef4d15281ad3d18ab9ced84ebe5d41bd450
launchpencil <launchpencil@gmail.com>
2023-04-02 13:36:47 +0900

    これで行けるかな…？

--- app/html/time/index.js
+++ app/html/time/index.js
@@ -25,7 +25,14 @@ function syncdata() {
     });
 
 
-    const customSelects = document.querySelectorAll('.table > div > div');
+    const customSelects = document.querySelectorAll('.table > div > div')[
+        7, 8, 9, 10, 11,
+        13, 14, 15, 16, 17,
+        19, 20, 21, 22, 23,
+        25, 26, 27, 28, 29,
+        31, 32, 33, 34, 35,
+        37, 38, 39, 40, 41
+      ];
 
     customSelects.forEach(customSelect => {
         const select = customSelect.querySelector("select");
